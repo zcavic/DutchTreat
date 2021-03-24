34b16afa2bbe3efd87b3bcd654aecc78996014ae
Zeljko Cavic <zeljko.cavic@schneider-electric-dms.com>
2021-03-24 14:49:18 +0100

    Added index.html

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -21,10 +21,8 @@ namespace DutchTreat
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			app.Run(async context => 
-			{
-				await context.Response.WriteAsync("<html><body><h1>Hello World</h1></body></html>");
-			});
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
 		}
 	}
 }
