14845df50fe0685eda76cf8ff4abf76160901be5
Zeljko Cavic <zeljko.cavic@schneider-electric-dms.com>
2021-03-28 13:45:38 +0200

    Event handling on button click

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -5,3 +5,8 @@ console.log("Hello Pluralsight");
 
 var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+	console.log("Buying Item");
+});
