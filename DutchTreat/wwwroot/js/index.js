9eff858bb05832dc8db45c7a2433be21f1e93ba4
Zeljko Cavic <zeljko.cavic@schneider-electric-dms.com>
2021-03-28 14:03:42 +0200

    The simple usage of jquery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -3,14 +3,16 @@ var s = "";
 
 console.log("Hello Pluralsight");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+var theForm = $("#theForm");
+theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
+var button = $("#buyButton");
+button.on("click", function () {
 	console.log("Buying Item");
 });
 
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].childern;
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+	console.log("You clicked on " + $(this).text());
+})
