7f6c108be2123d073c39d504352ff5852c2a65a1
zcavic <zeljkochavic@gmial.com>
2021-04-14 14:25:02 +0200

    Validation and ViewModel

--- DutchTreat/Controllers/OrdersController.cs
+++ DutchTreat/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using DutchTreat.Data;
 using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -53,13 +54,37 @@ namespace DutchTreat.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult Post([FromBody]Order model)
+		public IActionResult Post([FromBody]OrderViewModel model)
 		{
 			try
 			{
-				repository.AddEntity(model);
-				if(repository.SaveAll())
-					return Created($"/app/orders/{model.Id}", model);
+				if (ModelState.IsValid)
+				{
+					var newOrder = new Order()
+					{
+						OrderDate = model.OrderDate,
+						OrderNumber = model.OrderNumber,
+						Id = model.OrderId
+					};
+					if (newOrder.OrderDate == DateTime.MinValue)
+						newOrder.OrderDate = DateTime.Now;
+
+					repository.AddEntity(newOrder);
+					if (repository.SaveAll())
+					{
+						var vm = new OrderViewModel()
+						{
+							OrderId = newOrder.Id,
+							OrderDate = newOrder.OrderDate,
+							OrderNumber = newOrder.OrderNumber
+						};
+						return Created($"/app/orders/{vm.OrderId}", vm);
+					}
+				}
+				else
+				{
+					return BadRequest(ModelState);
+				}
 			}
 			catch (Exception ex)
 			{
