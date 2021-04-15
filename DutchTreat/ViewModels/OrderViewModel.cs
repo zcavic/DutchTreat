014f782434b74190d716360c8e2e5bb578b24b26
zcavic <zeljkochavic@gmial.com>
2021-04-15 21:43:01 +0200

    Creating Association Controllers

--- DutchTreat/ViewModels/OrderViewModel.cs
+++ DutchTreat/ViewModels/OrderViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DutchTreat.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -20,6 +21,11 @@ namespace DutchTreat.ViewModels
         [Required]
         [MinLength(4)]
         public string OrderNumber
+        {
+            get; set;
+        }
+
+        public ICollection<OrderItemViewModel> Items
         {
             get; set;
         }
