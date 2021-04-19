035ebfecfcffd345a5a53135a9a25aa231b0889b
zcavic <zeljkochavic@gmial.com>
2021-04-19 23:42:59 +0200

    Identity in API

--- DutchTreat/Controllers/AccountController.cs
+++ DutchTreat/Controllers/AccountController.cs
@@ -8,6 +8,12 @@ using Microsoft.AspNetCore.Identity;
 using DutchTreat.Data.Entities;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace DutchTreat.Controllers
 {
@@ -15,11 +21,15 @@ namespace DutchTreat.Controllers
 	{
 		private readonly ILogger<AccountController> logging;
 		private readonly SignInManager<StoreUser> signInManager;
+		private readonly UserManager<StoreUser> userManager;
+		private readonly IConfiguration config;
 
-		public AccountController(ILogger<AccountController> logging, SignInManager<StoreUser> signInManager)
+		public AccountController(ILogger<AccountController> logging, SignInManager<StoreUser> signInManager, UserManager<StoreUser> userManager, IConfiguration config)
 		{
 			this.logging = logging;
 			this.signInManager = signInManager;
+			this.userManager = userManager;
+			this.config = config;
 		}
 
 		public IActionResult Login()
@@ -55,6 +65,39 @@ namespace DutchTreat.Controllers
 		{
 			await signInManager.SignOutAsync();
 			return RedirectToAction("Index", "App");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateToken([FromBody] LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await userManager.FindByNameAsync(model.Username);
+				if (user != null)
+				{
+					var result = await signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+					if (result.Succeeded)
+					{
+						var claims = new[]
+						{
+							new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+							new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+							new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+						};
+
+						var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"]));
+						var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+						var token = new JwtSecurityToken(config["Token:Issuer"], config["Token:Audience"], claims, signingCredentials: creds, expires: DateTime.UtcNow.AddMinutes(20));
+
+						return Created("", new
+						{
+							token = new JwtSecurityTokenHandler().WriteToken(token),
+							expiration = token.ValidTo
+						} );
+					}
+				}
+			}
+			return BadRequest();
 		}
 	}
 }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -6,19 +6,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DutchTreat
 {
 	public class Startup
 	{
+		private readonly IConfiguration config;
+
+		public Startup(IConfiguration config)
+		{
+			this.config = config;
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -28,6 +38,18 @@ namespace DutchTreat
 				cfg.User.RequireUniqueEmail = true;
 			})
 				.AddEntityFrameworkStores<DutchContext>();
+
+			services.AddAuthentication()
+				.AddCookie()
+				.AddJwtBearer(cfg =>
+				{
+					cfg.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+					{
+						ValidIssuer = config["Token:Issuer"],
+						ValidAudience = config["Token:Audience"],
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"]))
+					};
+				});
 
 			services.AddDbContext<DutchContext>(cnf => 
 			{
